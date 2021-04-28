6151bef9374d07f1f5da7c018c704ad12748d0b1
unknown <anishkumarsarmi@gmail.com>
2021-04-28 12:21:35 +0530

    Commit

--- src/Components/Homepage/landingpage.jsx
+++ src/Components/Homepage/landingpage.jsx
@@ -1,39 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import FromCompo from "./formCompo";
-import NewsFeed from "./news";
 
 const Landing = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.REACT_APP_NEWS_API}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        setData(res.articles);
-      });
-  }, []);
-
   return (
     <div className="main-browser">
       <FromCompo />
-      <div className="news-feeds">
-        <div className="news-feeds-inner">
-          {data.map((article, index) => {
-            return (
-              <NewsFeed
-                key={index}
-                title={article.title}
-                url={article.url}
-                image={article.urlToImage}
-              />
-            );
-          })}
-        </div>
-      </div>
     </div>
   );
 };
