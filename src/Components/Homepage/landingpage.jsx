7eb8707dc3e671217e643712af7e7fa406c93c0c
unknown <anishkumarsarmi@gmail.com>
2021-04-28 13:08:05 +0530

    Added author name

--- src/Components/Homepage/landingpage.jsx
+++ src/Components/Homepage/landingpage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FromCompo from "./formCompo";
+import Footer from "../Footer";
 
 const Landing = () => {
   return (
     <div className="main-browser">
       <FromCompo />
+      <Footer />
     </div>
   );
 };
